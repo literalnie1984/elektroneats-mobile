f34a5e7555a3dd7ffd4feb442a9b27536a40d2dd
Polaczeq22 <mikulamichal@pm.me>
2023-03-23 23:23:51 +0100

    Added simple ui preview

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MainView from './views/MainView';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+		<MainView />
+		<StatusBar style="auto" translucent={false} hidden={false} animated={true} />
+	</NavigationContainer>
   );
 }
 
---
+++ views/MainView.tsx
@@ -0,0 +1,131 @@
+import { BottomTabHeaderProps, createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { View, Text, StyleSheet, TextStyle, RegisteredStyle, ViewStyle } from 'react-native';
+import { faUtensils, faTableList, faBasketShopping, faEllipsisVertical, faStore } from "@fortawesome/free-solid-svg-icons";
+import {icon} from '@fortawesome/fontawesome-svg-core';
+
+const Tab = createBottomTabNavigator();
+
+const OrdersScreen = () => {
+		return(
+				<View style={style.container}>
+						<Text>Orders</Text>
+				</View>
+		);
+};
+
+const MenuScreen = () => {
+		return(
+				<View style={style.container}>
+						<Text>Menu</Text>
+				</View>
+		);
+};
+
+const ShopScreen = () => {
+		return(
+				<View style={style.container}>
+						<Text>Shop</Text>
+				</View>
+		);
+};
+
+const CartScreen = () => {
+		return(
+				<View style={style.container}>
+						<Text>Shopping cart</Text>
+				</View>
+		);
+};
+
+const SettingsScreen = () => {
+		return(
+				<View style={style.container}>
+						<Text>Settings</Text>
+				</View>
+		 );
+};
+
+interface HeaderProps {
+		style: RegisteredStyle<ViewStyle>, 
+		titleStyle: RegisteredStyle<TextStyle>,
+		title: string,
+}
+
+const HeaderView = (props: HeaderProps) => {
+		return(
+		<View style={props.style}>
+				<Text>K</Text>
+				<Text style={props.titleStyle}>{props.title}</Text>
+				<Text>L</Text>
+		</View>
+		);
+};
+
+const MainView = () => {
+		return(
+				<Tab.Navigator
+				screenOptions={({route}) => ({
+						header: ({ navigation, route, options }: BottomTabHeaderProps) => {
+								return <HeaderView title={route.name} style={options.headerStyle} titleStyle={options.headerTitleStyle}/>
+						},
+						headerStyle: {
+								height: 60,
+								flexDirection: "row",
+								flexWrap: "nowrap",
+								backgroundColor: "#fff",
+								alignItems: "center",
+								justifyContent: "space-between",
+								paddingHorizontal: 30,
+						},
+						headerTitleStyle: {
+								textAlign: "center",
+								fontWeight: "bold",
+								fontSize: 16,
+						}, 
+						tabBarIcon: ({focused, color, size}) => {
+								let iconName;
+								switch(route.name){
+										case "Orders":{
+												iconName=faTableList;
+										} break;
+										case "Menu":{
+												iconName=faUtensils;
+										} break;
+										case "Shop":{
+												iconName=faStore;
+										} break;
+										case "Cart":{
+												iconName=faBasketShopping;
+										} break;
+										case "Settings":{
+												iconName=faEllipsisVertical;
+										} break;
+								};
+
+								return <FontAwesomeIcon icon={iconName} color={color} size={size} /> 
+						},
+				})}
+				>
+						<Tab.Screen name="Orders" component={OrdersScreen} />
+						<Tab.Screen name="Menu" component={MenuScreen} />
+						<Tab.Screen name="Shop" component={ShopScreen} />
+						<Tab.Screen name="Cart" component={CartScreen} />
+						<Tab.Screen name="Settings" component={SettingsScreen} />
+				</Tab.Navigator>
+		);
+};
+
+const style = StyleSheet.create({
+		container: {
+				flex: 1,
+				padding: "auto",
+				justifyContent: "center",
+				alignItems: "center",
+		},
+		text:{
+				
+		},
+});
+
+export default MainView;
