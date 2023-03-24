f61ffa9985a27471459466215bd8da13eec0579d
Polaczeq22 <mikulamichal@pm.me>
2023-03-24 15:24:52 +0100

    Added bigger Account "logo" - easier action calling
    Removed views/MainView.tsx

--- src/views/HeaderView.tsx
+++ src/views/HeaderView.tsx
@@ -1,16 +1,28 @@
-import { View, Text, Pressable } from "react-native";
-import { HeaderProps } from "../types";
+import { View, Text, Pressable } from 'react-native';
+import { Svg, Circle, Text as SvgText } from 'react-native-svg';
+import { HeaderProps } from '../types';
 
 const HeaderView = (props: HeaderProps) => {
-  return (
-    <View style={props.style}>
-      <Text>K</Text>
-      <Text style={props.titleStyle}>{props.title}</Text>
-      <Pressable onPress={() => props.stackNavigation.navigate("AccountView")}>
-        <Text>L</Text>
-      </Pressable>
-    </View>
-  );
+		return(
+		<View style={props.style}>
+				<Text>K</Text>
+				<Text style={props.titleStyle}>{props.title}</Text>
+				<Pressable onPress={() => props.stackNavigation.navigate("AccountView")}>
+						<Svg height={32} width={32}>
+								<Circle cx={16} cy={16} r={16} fill={"pink"} />
+								<SvgText 
+										x={16} 
+										y={20} 
+										textAnchor={'middle'} 
+										fontSize={16} 
+										stroke={"black"}
+								>
+										L
+								</SvgText>
+						</Svg>
+				</Pressable>
+		</View>
+		);
 };
 
 export default HeaderView;
