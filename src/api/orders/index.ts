ad10c3296f4161daac4611fabe16b792974cfde3
M4rceleq <maciaszczyk.marcel@gmail.com>
2023-03-31 00:49:44 +0200

    create return types for order endpoints

--- src/api/orders/index.ts
+++ src/api/orders/index.ts
@@ -1,19 +1,18 @@
-import { fetchWithJWT } from "../fetch";
+import { fetchForSuccess, fetchWithJWT } from "../fetch";
+import { FetchedDinner } from "../menu/types";
 import { JWT } from "../types";
 import { CreateOrdersBody } from "./types";
 
-// TODO: create return types
+const createOrders = async (token: JWT, body: CreateOrdersBody): Promise<boolean> => (
+  fetchForSuccess({ path: `orders/create`, method: "POST", token, body })
+);
 
-const createOrders = async (token: JWT, body: CreateOrdersBody): Promise<unknown> => {
-  return await fetchWithJWT({ path: `orders/create`, method: "POST", token, body });
-};
+const getPendingUserOrders = async (token: JWT): Promise<FetchedDinner[] | null> => (
+  fetchWithJWT({ path: `orders/pending-user-orders`, method: "GET", token })
+);
 
-const getPendingUserOrders = async (token: JWT): Promise<unknown> => {
-  return await fetchWithJWT({ path: `orders/create`, method: "GET", token });
-};
-
-const getCompletedUserOrders = async (token: JWT): Promise<unknown> => {
-  return await fetchWithJWT({ path: `orders/create`, method: "GET", token });
-};
+const getCompletedUserOrders = async (token: JWT): Promise<FetchedDinner[] | null> => (
+  fetchWithJWT({ path: `orders/completed-user-orders`, method: "GET", token })
+);
 
 export default { createOrders, getPendingUserOrders, getCompletedUserOrders };
